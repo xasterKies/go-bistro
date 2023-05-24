a185492bc504b94cdb93f81832f86104a3e09c79
xasterKies <samueledward40@gmail.com>
2023-05-24 21:26:25 +0100

    chore: setting up main file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xasterKies/go-bistro
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"github.com/xasterKies/go-bistro/database"
+	"github.com/xasterKies/routes"
+	"github.com/xasterKies/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/gin-gonic/gin"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	routes.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+}
