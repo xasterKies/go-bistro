df728ee1fe1f137f82c7d461b802909fcbfe87d0
xasterKies <samueledward40@gmail.com>
2023-05-26 04:05:05 +0100

    feat: complete menuModel

--- models/menuModel.go
+++ models/menuModel.go
@@ -6,14 +6,13 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-
 type Menu struct {
-	ID primitive.ObjectID `bson:"_id"`
-	Name string `json:"name" validate:"required"`
-	Category string `json:"category" validate:"required"`
-	Start_Date *time.Time `json:"start_date"`
-	End_Date *time.Time `json:"end_date`
-	Created_at time.Time ``
-	Updated_at time.Time
-	Menu_id string
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       string             `json:"name" validate:"required"`
+	Category   string             `json:"category" validate:"required"`
+	Start_Date *time.Time         `json:"start_date"`
+	End_Date   *time.Time         `json:"end_date`
+	Created_at time.Time          `json:"created_at`
+	Updated_at time.Time          `json:updated_at`
+	Menu_id    string             `json:food_id`
 }
