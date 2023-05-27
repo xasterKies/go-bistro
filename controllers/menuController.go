e957234352179c0ea42f882524c8cb5374c6ae1a
xasterKies <samueledward40@gmail.com>
2023-05-27 07:52:47 +0100

    chore: inTimeSpan func

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -82,6 +82,10 @@ func CreateMenu() gin.HandlerFunc {
 	}
 }
 
+func inTimeSpan(start, end, check time.Time) bool {
+	return start.After(time.Now()) && end.After(start)
+}
+
 func UpdateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
