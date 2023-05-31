09f218a573bd3f5424d251cdd0bf19479f01ee3b
xasterKies <samueledward40@gmail.com>
2023-05-31 20:31:38 +0100

    feat: completed authmiddleware

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,12 +1,33 @@
 package middleware
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-	
+	"github.com/xasterKies/go-bistro/helpers"
 )
 
 func Authentication() gin.HandlerFunc{
 	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helpers.validateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
 
 	}
 }
